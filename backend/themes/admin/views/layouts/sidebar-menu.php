528a8dc43ed723249c41900d67a13394399ec1d5
JunioSantos <junio.p@gmail.com>
2014-09-02 17:24:34 -0300

    correção do menu

--- backend/themes/admin/views/layouts/sidebar-menu.php
+++ backend/themes/admin/views/layouts/sidebar-menu.php
@@ -16,59 +16,8 @@ if (!empty($menus)) {
             'options' => [
                 'class' => 'sidebar-menu',
             ],
+            'linkTemplate' => '<a href="{url}">{label}</a>',
             'items' => $menus,
         ]
     );
 }
-
-echo Menu::widget(
-    [
-        'options' => [
-            'class' => 'sidebar-menu'
-        ],
-        'items' => [
-            [
-                'label' => Yii::t('themes', 'Dashboard'),
-                'url' => Yii::$app->homeUrl,
-                'icon' => 'fa-dashboard',
-                'active' => Yii::$app->request->url === Yii::$app->homeUrl
-            ],
-            [
-                'label' => Yii::t('themes', 'Users'),
-                'url' => ['/users/default/index'],
-                'icon' => 'fa-group'
-            ],
-            [
-                'label' => Yii::t('themes', 'Assigment'),
-                'url' => ['/admin/assigment'],
-                'icon' => 'fa-group'
-            ],
-            [
-                'label' => Yii::t('themes', 'Role'),
-                'url' => ['/admin/role'],
-                'icon' => 'fa-group'
-            ],
-            [
-                'label' => Yii::t('themes', 'Permission'),
-                'url' => ['/admin/permission'],
-                'icon' => 'fa-group'
-            ],
-            [
-                'label' => Yii::t('themes', 'Route'),
-                'url' => ['/admin/route'],
-                'icon' => 'fa-group'
-            ],
-            [
-                'label' => Yii::t('themes', 'Rule'),
-                'url' => ['/admin/rule'],
-                'icon' => 'fa-group'
-            ],
-            [
-                'label' => Yii::t('themes', 'Menu'),
-                'url' => ['/admin/menu'],
-                'icon' => 'fa-group',
-            ],
-
-        ]
-    ]
-);
